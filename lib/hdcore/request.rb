6637577fa8464920a7abc543cb5cfeb93b866f02
Ethan Pemble <ethan.p@hostdime.com>
2013-05-10 16:39:24 +0000

    hid internal request methods

--- lib/hdcore/request.rb
+++ lib/hdcore/request.rb
@@ -5,8 +5,18 @@ module Hdcore
 
     class << self
 
-      # Initializes the request: throws exception if there are configs missing (requires public and private keys)
       # @param [String] action The full API action
+      # @param [Hash] params The given action parameters
+      # @return [HTTParty::Response]
+      def call(action, params = {})
+        init()
+        self.get("/call/api_action/#{action}/format/json/", query_string(action, params))
+      end
+
+      private
+
+      # @private
+      # Initializes the request: throws exception if there are configs missing (requires public and private keys)
       def init
         # Make sure required config values are set
         if Hdcore.missing_config_values?
@@ -20,32 +30,30 @@ module Hdcore
         base_uri Hdcore.config[:api_endpoint]
       end
 
+      # @private
       # @return [String] public key established in configuration
       def public_key
         Hdcore.config[:public_key]
       end
 
+      # @private
       # @return [String] private key established in configuration
       def private_key
         Hdcore.config[:private_key]
       end
 
+      # @private
       # @param [String] action The full API action
-      # @param [Hash] params The given action parameters
-      # @return [HTTParty::Response]
-      def send(action, params = {})
-        init()
-        self.get("/call/api_action/#{action}/format/json/", query_string(action, params))
-      end
-
       # @param [Hash] params The given action parameters
       # @return [Hash] Full set of parameters, including generated api parameters
       def query_string(action, params = {})
         params.merge generate_api_params(action, params)
       end
 
+      # @private
+      # @param [String] action The full API action
       # @param [Hash] action_params The given action parameters
-      # @return [Hash] required api parameters: [api_key, api_unique, api_timestamp, api_hash]
+      # @return [Hash] required api parameters: {:api_key, :api_unique, :api_timestamp, :api_hash}
       def generate_api_params(action, params = {})
         {
           api_key:       public_key,
@@ -59,11 +67,13 @@ module Hdcore
         }
       end
 
+      # @private
       # @return [String] SHA256 hash of all the arguments "joined:with:colons"
       def generate_hash(*args)
         (Digest::SHA2.new << args.join(':')).to_s
       end
 
+      # @private
       # @return [String] Version 4 UUID
       # More: http://en.wikipedia.org/wiki/Universally_unique_identifier#Version_4_.28random.29
       def generate_uuid
--- spec/lib/hdcore/request_spec.rb
+++ spec/lib/hdcore/request_spec.rb
@@ -2,13 +2,13 @@ require_relative '../../../lib/hdcore'
 
 describe Hdcore::Request do
 
-  describe '.send' do
+  describe '.call' do
     it 'initializes and sends GET request to API endpoint' do
       test_action = 'some.action'
       Hdcore::Request.stub(:query_string).and_return(params = {some: 'params'})
       Hdcore::Request.should_receive(:init)
       Hdcore::Request.should_receive(:get).with("/call/api_action/#{test_action}/format/json/", params)
-      Hdcore::Request.send(test_action, {})
+      Hdcore::Request.call(test_action, {})
     end
   end
 
@@ -16,7 +16,7 @@ describe Hdcore::Request do
   describe '.query_string' do
     it 'returns parameters merged with generated api parameters' do
       Hdcore::Request.stub(:generate_api_params).and_return(api_params = {some: 'api_params'})
-      actual = Hdcore::Request.query_string('some.action', params = {some_other: 'params'})
+      actual = Hdcore::Request.send(:query_string, 'some.action', params = {some_other: 'params'})
       actual.should == params.merge(api_params)
     end
   end
@@ -24,31 +24,31 @@ describe Hdcore::Request do
 
   describe '.generate_api_params' do
     it 'returns hash with four valid param keys' do
-      actual = Hdcore::Request.generate_api_params("", {})
+      actual = Hdcore::Request.send(:generate_api_params, "", {})
       actual.keys.should == [:api_key, :api_unique, :api_timestamp, :api_hash]
     end
 
     it 'returns [:api_key] => public key' do
       Hdcore::Request.stub(:public_key).and_return(key = 'some_public_key')
-      actual = Hdcore::Request.generate_api_params("", {})
+      actual = Hdcore::Request.send(:generate_api_params, "", {})
       actual[:api_key].should == key
     end
 
     it 'returns [:api_unique] => generated uuid' do
       Hdcore::Request.stub(:generate_uuid).and_return(uuid = 'some_uuid')
-      actual = Hdcore::Request.generate_api_params("", {})
+      actual = Hdcore::Request.send(:generate_api_params, "", {})
       actual[:api_unique].should == uuid
     end
 
     it 'returns [:api_timestamp] => time of execution' do
       timestamp = Time.now.to_i
-      actual = Hdcore::Request.generate_api_params("", {})
+      actual = Hdcore::Request.send(:generate_api_params, "", {})
       actual[:api_timestamp].should == timestamp
     end
 
     it 'returns [:api_hash] => generated hash' do
       Hdcore::Request.stub(:generate_hash).and_return(hash = 'some_hash')
-      actual = Hdcore::Request.generate_api_params("", {})
+      actual = Hdcore::Request.send(:generate_api_params, "", {})
       actual[:api_hash].should == hash
     end
 
@@ -64,7 +64,7 @@ describe Hdcore::Request do
                                                           (params = {some: 'optional_params'}).to_json
                                                         )
 
-      Hdcore::Request.generate_api_params(action, params)
+      Hdcore::Request.send(:generate_api_params, action, params)
     end
   end
 
@@ -73,14 +73,14 @@ describe Hdcore::Request do
     it 'uses the SHA256 algorithm, and joins parameters with colons' do
       args = %w[one two three 4 five seven 8]
       hash = (Digest::SHA2.new << args.join(':')).to_s
-      Hdcore::Request.generate_hash(*args).should == hash
+      Hdcore::Request.send(:generate_hash, args).should == hash
     end
   end
 
   describe '.generate_uuid' do
     it 'uses Version 4 UUID' do
       SecureRandom.should_receive(:uuid).and_return(uuid = 'some_uuid')
-      Hdcore::Request.generate_uuid.should == uuid
+      Hdcore::Request.send(:generate_uuid).should == uuid
     end
   end
 end
